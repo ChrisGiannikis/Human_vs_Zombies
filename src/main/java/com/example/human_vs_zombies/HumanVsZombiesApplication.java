93a7007e74887b2b02f4cb25ef699f57615cce15
Christos <xrist.giann@gmail.com>
2023-03-09 17:58:25 +0200

    Initial commit!

---
+++ src/main/java/com/example/human_vs_zombies/HumanVsZombiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.human_vs_zombies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanVsZombiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumanVsZombiesApplication.class, args);
+    }
+
+}
