1e9cf0e2270c54e6934a51005050d0e71d6fb6b5
Christos <xrist.giann@gmail.com>
2023-03-11 14:56:27 +0200

    FEAT: added PlayerAdminDTO for admins to manipulate data from players and PlayerSimpleDTO to manipulate non-sensitive data from players.

---
+++ src/main/java/com/example/human_vs_zombies/api_04_Player/dto/PlayerSimpleDTO.java
@@ -0,0 +1,17 @@
+package com.example.human_vs_zombies.api_04_Player.dto;
+
+import com.example.human_vs_zombies.entities.AppUser;
+import com.example.human_vs_zombies.entities.Kill;
+import com.example.human_vs_zombies.entities.SquadMember;
+import lombok.Getter;
+import lombok.Setter;
+
+/* This DTO created to get not sensitive player details */
+@Getter
+@Setter
+public class PlayerSimpleDTO {
+    private int player_id;
+    private AppUser user;
+    private Kill death;
+    private SquadMember squadMember;
+}
