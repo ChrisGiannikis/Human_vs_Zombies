e2207ace24577a08eba5d8011c2cc2b34ab8742c
Anastasios Kamperopoulos <anastasioskamperopoulos@gmail.com>
2023-03-11 16:15:53 +0200

    Added method to Kill repository to find Kill ,by kill_id.

--- src/main/java/com/example/human_vs_zombies/repositories/KillRepository.java
+++ src/main/java/com/example/human_vs_zombies/repositories/KillRepository.java
@@ -8,4 +8,5 @@ import java.util.ArrayList;
 public interface KillRepository extends JpaRepository<Kill,Integer> {
         ArrayList<Kill> findAllKillsByGameId(int game_id);
 
+        Kill findKillByKillId(int kill_id);
 }
