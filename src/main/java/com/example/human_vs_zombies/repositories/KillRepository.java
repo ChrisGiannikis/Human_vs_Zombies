4c3e4b6a73ded365b9578d290ac6d93608951b84
Anastasios Kamperopoulos <anastasioskamperopoulos@gmail.com>
2023-03-11 16:12:42 +0200

    Added method to get All Kills.

--- src/main/java/com/example/human_vs_zombies/repositories/KillRepository.java
+++ src/main/java/com/example/human_vs_zombies/repositories/KillRepository.java
@@ -1,4 +1,11 @@
 package com.example.human_vs_zombies.repositories;
 
-public interface KillRepository {
+import com.example.human_vs_zombies.entities.Kill;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.ArrayList;
+
+public interface KillRepository extends JpaRepository<Kill,Integer> {
+        ArrayList<Kill> findAllKillsByGameId(int game_id);
+
 }
