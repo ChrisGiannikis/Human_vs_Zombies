0863c479d06c9580b48bfdf1f3af767960726b6d
Anastasios Kamperopoulos <anastasioskamperopoulos@gmail.com>
2023-03-13 12:11:59 +0200

    added basic functionalities to killServiceImpl and KillController

--- src/main/java/com/example/human_vs_zombies/repositories/KillRepository.java
+++ src/main/java/com/example/human_vs_zombies/repositories/KillRepository.java
@@ -6,7 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.ArrayList;
 
 public interface KillRepository extends JpaRepository<Kill,Integer> {
-        ArrayList<Kill> findAllKillsByGameId(int game_id);
-
-        Kill findKillByKillId(int kill_id);
 }
--- src/main/java/com/example/human_vs_zombies/services/kill/KillService.java
+++ src/main/java/com/example/human_vs_zombies/services/kill/KillService.java
@@ -2,6 +2,9 @@ package com.example.human_vs_zombies.services.kill;
 
 import com.example.human_vs_zombies.entities.Kill;
 import com.example.human_vs_zombies.services.CrudService;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface KillService extends CrudService<Kill,Integer> {
+    public Kill updateKillById(Kill kill,int id);
 }
