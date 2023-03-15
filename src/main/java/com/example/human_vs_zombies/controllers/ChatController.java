f16ff285a78ed1bedd2e18d36afac34df2217b9e
Aris Kardasis <arisran1@hotmail.com>
2023-03-15 14:18:53 +0200

    FEAT: Created DTO and Mapper for Chat, used them in Controller

--- src/main/java/com/example/human_vs_zombies/controllers/ChatController.java
+++ src/main/java/com/example/human_vs_zombies/controllers/ChatController.java
@@ -1,6 +1,7 @@
 package com.example.human_vs_zombies.controllers;
 
-import com.example.human_vs_zombies.entities.Chat;
+import com.example.human_vs_zombies.dto.ChatDTO;
+import com.example.human_vs_zombies.mappers.ChatMapper;
 import com.example.human_vs_zombies.services.chat.ChatService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,40 +14,44 @@ import java.util.Collection;
 public class ChatController {
 
     private final ChatService chatService;
+    private final ChatMapper chatMapper;
 
-    public ChatController(ChatService chatService){
+    public ChatController(ChatService chatService, ChatMapper chatMapper){
         this.chatService = chatService;
+        this.chatMapper = chatMapper;
     }
 
-    @GetMapping("{id}")//GET: localhost:8080/api/v1/chats/id
-    public ResponseEntity<Chat> getById(@PathVariable int id){
-        return ResponseEntity.ok(chatService.findById(id));
-    }
 
     @GetMapping//GET: localhost:8080/api/v1/chats
-    public ResponseEntity<Collection<Chat>> getAll(){
-        return ResponseEntity.ok(chatService.findAll());
+    public ResponseEntity<Collection<ChatDTO>> getAll(){
+        Collection<ChatDTO> chatDTOS = chatMapper.chatToChatDto(chatService.findAll());
+        return ResponseEntity.ok(chatDTOS);
+    }
+
+
+    @GetMapping("{id}")//GET: localhost:8080/api/v1/chats/id
+    public ResponseEntity<ChatDTO> getById(@PathVariable int id){
+        ChatDTO chatDTO = chatMapper.chatToChatDto(chatService.findById(id));
+        return ResponseEntity.ok(chatDTO);
     }
 
     @PostMapping // POST: localhost:8080/api/v1/chats
-    public ResponseEntity<Chat> add(@RequestBody Chat chat) {
-        Chat ch = chatService.add(chat);
-        URI location = URI.create("chats/" + ch.getMessage_id());
+    public ResponseEntity<ChatDTO> add(@RequestBody ChatDTO chatDTO) {
+        chatService.add(chatMapper.chatDtoToChat(chatDTO));
+        URI location = URI.create("chats/" + chatDTO.getMessage_id());
         return ResponseEntity.created(location).build();
-        // return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PutMapping("{id}") // POST: localhost:8080/api/v1/chats
-    public ResponseEntity<Chat> update(@RequestBody Chat chat, @PathVariable int id) {
-        // Validates if body is correct
-        if(id != chat.getMessage_id())
+    public ResponseEntity<ChatDTO> update(@RequestBody ChatDTO chatDTO, @PathVariable int id) {
+        if(id != chatDTO.getMessage_id())
             return ResponseEntity.badRequest().build();
-        chatService.update(chat);
+        chatService.update(chatMapper.chatDtoToChat(chatDTO));
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("{id}") // DELETE: localhost:8080/api/v1/chats/id
-    public ResponseEntity<Chat> delete(@PathVariable int id) {
+    public ResponseEntity<ChatDTO> delete(@PathVariable int id) {
         chatService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
