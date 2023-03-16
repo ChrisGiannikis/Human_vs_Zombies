0688f4090948111abed6b488069e7eec7897eaef
Anastasios Kamperopoulos <anastasioskamperopoulos@gmail.com>
2023-03-16 14:33:34 +0200

    Added: Swagger Documentation to KillController, and refactor some crud functionalities.

--- src/main/java/com/example/human_vs_zombies/controllers/KillController.java
+++ src/main/java/com/example/human_vs_zombies/controllers/KillController.java
@@ -2,11 +2,20 @@ package com.example.human_vs_zombies.controllers;
 
 import com.example.human_vs_zombies.dto.kill.KillDTO;
 import com.example.human_vs_zombies.dto.kill.KillPostDTO;
+import com.example.human_vs_zombies.dto.player.PlayerAdminDTO;
+import com.example.human_vs_zombies.dto.player.PlayerSimpleDTO;
 import com.example.human_vs_zombies.entities.Kill;
 import com.example.human_vs_zombies.mappers.KillMapper;
 import com.example.human_vs_zombies.services.game.GameService;
 import com.example.human_vs_zombies.services.kill.KillService;
 import com.example.human_vs_zombies.services.player.PlayerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import static java.util.Objects.isNull;
@@ -29,13 +38,35 @@ public class KillController {
     }
 
     //get all kills
+    @Operation(summary = "Get all the Kills of the game.")
+    @ApiResponses( value = {
+            @ApiResponse(responseCode = "200", description = "Success",
+                    content = {@Content( mediaType = "application/json",
+                            array = @ArraySchema( schema = @Schema(implementation = KillDTO.class)))})
+    })
+
+
+
 
     @GetMapping("/kill")
     public ResponseEntity findAll(){
         return ResponseEntity.ok(killMapper.killsToKillsDTO(killService.findAll()));
     }
 
+
+
     //get kill by id
+    @Operation(summary = "Get a kill with the given id.")
+    @ApiResponses( value = {
+            @ApiResponse(responseCode = "200", description = "Success",
+                    content = {@Content( mediaType = "application/json",
+                            schema = @Schema(implementation = KillDTO.class))}),
+            @ApiResponse( responseCode = "404",
+                    description = "Kill with supplied id, does not exist! ",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ProblemDetail.class)))
+    })
+
     @GetMapping("/kill/{kill_id}")
     public ResponseEntity findById(@PathVariable("kill_id") int id){
         return ResponseEntity.ok(killMapper.killToKillDTO(killService.findById(id)));
@@ -43,20 +74,50 @@ public class KillController {
 
     // create a kill
 
-    @PostMapping("/kill")
-    public ResponseEntity createKill(@RequestBody KillPostDTO kill){
 
-        return ResponseEntity.ok(killService.add(killMapper.KillPostDTOToKill(kill)));
+
+    @Operation(summary = "Creates a kill object.")
+    @ApiResponses( value = {
+            @ApiResponse( responseCode = "201", description = "Kill object created", content = { @Content }),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = { @Content }),
+            @ApiResponse( responseCode = "404", description = "Kill not found", content = { @Content })
+    })
+    @PostMapping("/kill")
+    public ResponseEntity createKill(@RequestBody Kill kill){
+
+        return ResponseEntity.ok(killService.add(killMapper.killToKillPostDTO(kill)));
 
     }
+
 
     //update a kill
+
+    @Operation(summary = "Updates the kill object with the given id.")
+    @ApiResponses( value = {
+            @ApiResponse( responseCode = "204", description = "Kill updated", content = { @Content }),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = { @Content }),
+            @ApiResponse( responseCode = "404", description = "Kill not found", content = { @Content })
+    })
     @PutMapping("/kill/{kill_id}")
-    public ResponseEntity updateKillById(@RequestBody KillDTO kill, @PathVariable("kill_id") int id){
-        return ResponseEntity.ok((killService.update(killMapper.killDTOToKill(kill))));
+    public ResponseEntity updateKill(@RequestBody KillDTO killDTO, @PathVariable("kill_id") int id){
+        Kill kill = killMapper.killDTOToKill(killDTO);
+        KillDTO updatedDTO = killMapper.killToKillDTO(killService.update(kill));
+        return ResponseEntity.ok(updatedDTO);
+
 
     }
 
+
+    @Operation(summary = "Deletes the kill object with the given id.")
+    @ApiResponses(value = {
+            @ApiResponse( responseCode =  "200",
+                    description = "Kill deleted",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = KillDTO.class))}),
+            @ApiResponse( responseCode = "404",
+                    description = "Player with supplied id, does not exist! ",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ProblemDetail.class)))})
     @DeleteMapping("/kill/{kill_id}")
     public ResponseEntity deleteKillById(@PathVariable("kill_id") int id){
         if(isNull(killService.findById(id)))
--- src/main/java/com/example/human_vs_zombies/mappers/KillMapper.java
+++ src/main/java/com/example/human_vs_zombies/mappers/KillMapper.java
@@ -1,18 +1,9 @@
 package com.example.human_vs_zombies.mappers;
 
-import com.example.human_vs_zombies.dto.GameDTO;
 import com.example.human_vs_zombies.dto.kill.KillDTO;
 import com.example.human_vs_zombies.dto.kill.KillPostDTO;
-import com.example.human_vs_zombies.entities.Game;
 import com.example.human_vs_zombies.entities.Kill;
-import com.example.human_vs_zombies.entities.Player;
-import com.example.human_vs_zombies.services.game.GameService;
-import com.example.human_vs_zombies.services.player.PlayerService;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
-import org.mapstruct.Named;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Collection;
 
@@ -24,7 +15,7 @@ public interface KillMapper {
      KillDTO killToKillDTO(Kill kill);
     Collection<Kill> killsToKillsDTO(Collection<Kill> kills);
      Kill killDTOToKill(KillDTO killDTO);
-    KillPostDTO killToKillPostDTO(Kill kill);
+    Kill killToKillPostDTO(Kill kill);
      Kill KillPostDTOToKill(KillPostDTO killPostDTO);
 
 }
