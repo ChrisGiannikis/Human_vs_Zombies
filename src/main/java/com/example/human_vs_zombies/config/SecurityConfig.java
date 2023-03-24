654b38842504a0fec3ebc343e055748a87c5e036
Christos <xrist.giann@gmail.com>
2023-03-24 09:51:35 +0200

    FIX: fixed requestMachers at security configuration.

--- src/main/java/com/example/human_vs_zombies/config/SecurityConfig.java
+++ src/main/java/com/example/human_vs_zombies/config/SecurityConfig.java
@@ -20,9 +20,9 @@ public class SecurityConfig {
                 .authorizeHttpRequests( authorize -> authorize
                         //define the mappings
                         //api/v1/resources/public 200
-                        .requestMatchers("api/v1/resources/public").permitAll()
+                        .requestMatchers("/api/v1/resources/public").permitAll()
                         //api/v1/resources/authorized 403
-                        .requestMatchers("api/v1/resources/authorized").hasRole("ADMIN")
+                        .requestMatchers("/api/v1/resources/authorized").hasRole("ADMIN")
                         //api/v1/resources/authenticated 401
                         .anyRequest().authenticated()
                 ).oauth2ResourceServer()
